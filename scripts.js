51f942da0bd414a16195fa3f3c08cfef2ab745b2
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-09-15 13:28:11 +0700

    Add new book button and functionality

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
+let myLibrary = [
+    {
+        title: 'The Hobbit'
+    },
+    {
+        title: 'Lord of the Rings'
+    }
+];
+
 function Book(title){
     this.title = title;
 }
 
-function addBookToLibrary(title){
-    const newBookDiv = document.createElement("p");
-    document.getElementById("bookList").appendChild(newBookDiv);
-
-    newBookDiv.innerHTML = title;
+function addBookToLibrary(){
+    myLibrary.push(new Book(document.getElementById('ftitle').value))
+    updateBookList();
 }
 
-const theHobbit = new Book('The Hobbit');
-const LotR = new Book('LotR');
+function updateBookList(){
+    //Clear book list DOM
+    while (document.querySelector('#bookList').firstChild){
+        document.querySelector('#bookList').removeChild(document.querySelector('#bookList').firstChild)
+    }
 
-let myLibrary = [theHobbit, LotR];
+    //Draw book list
+    for (let i = 0; i < myLibrary.length; i++){
+        const bookDiv = document.createElement("div");
+        bookDiv.className = "bookDiv"
+        document.getElementById("bookList").appendChild(bookDiv);
 
-console.log(theHobbit.title)
-for (let i = 0; i < myLibrary.length; i++){
-    addBookToLibrary(myLibrary[i].title);
+        const bookTitle = document.createElement("p");
+        bookDiv.appendChild(bookTitle);
+        bookTitle.innerHTML = "Title : " + myLibrary[i].title;
+    }    
 }
 
+updateBookList();
