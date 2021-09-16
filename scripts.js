83e6c41fb332206975eff779b084a586f8ea82e7
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-09-16 12:16:31 +0700

    Added delete button functionality

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
 let myLibrary = [
     {
+        id: 176432837,
         title: 'The Hobbit'
     },
     {
+        id: 329874329,
         title: 'Lord of the Rings'
     }
 ];
 
-function Book(title){
+function Book(id, title){
+    this.id = id;
     this.title = title;
 }
 
 function addBookToLibrary(){
-    myLibrary.push(new Book(document.getElementById('ftitle').value))
+    let bookID = uuid();
+    let bookTitle = document.getElementById('ftitle').value;
+
+    myLibrary.push(new Book(bookID, bookTitle));
     updateBookList();
 }
 
@@ -31,7 +37,32 @@ function updateBookList(){
         const bookTitle = document.createElement("p");
         bookDiv.appendChild(bookTitle);
         bookTitle.innerHTML = "Title : " + myLibrary[i].title;
+
+        const bookDelete = document.createElement("button");
+        bookDiv.appendChild(bookDelete);
+        bookDelete.innerHTML = "Delete Book"
+        bookDelete.addEventListener("click", () => {
+            myLibrary.splice(myLibrary.findIndex(x => x.id == myLibrary[i].id), 1);
+            updateBookList();
+        })
     }    
 }
 
 updateBookList();
+
+//Generate UUID for book identification
+function uuid() {    
+    var uuid = "", i, random;    
+
+    for (i = 0; i < 32; i++) {      
+        random = Math.random() * 16 | 0;        
+
+        if (i == 8 || i == 12 || i == 16 || i == 20) {        
+            uuid += "-";      
+        }
+
+        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);    
+     }   
+
+     return uuid;  
+}
